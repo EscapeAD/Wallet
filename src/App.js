4f0b7c7b0cf1cea69ee5774c8e5a5c98e389d62d
Adam Tse <mr.adam.tse@gmail.com>
2017-07-19 00:06:58 -0400

    changed comments to make it more clear

--- src/App.js
+++ src/App.js
@@ -1,21 +1,91 @@
 import '../shim';
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Header, Button, CardSection } from './components/common';
+import { Header, Button, CardSection, Card } from './components/common';
 import Bitcoin from 'react-native-bitcoinjs-lib';
 
 export default class App extends Component {
-  state = {}
+  state = {bitcoinAdresss: 'none', keypairdata: 'none'}
+
+  // renderbase(){
+  //   const bitcoinAdresss = this.state;
+  //   console.log(bitcoinAdresss)
+  // 
+  //   if(bitcoinAdresss != 'none'){
+  //     return (
+  //       <CardSection>
+  //         <Button onPress={()=> {this.setState({bitcoinAdresss: 'WTF'})
+  //   }}>
+  //           Create bitcoin key
+  //         </Button>
+  //       </CardSection>
+  //     )
+  //   } else {
+  //     return (
+  //       <Card>
+  //       <CardSection style={styles.container} >
+  //         <Text style={styles.instructions}>
+  //           Current Private key: {this.state.bitcoinAdresss}
+  //         </Text>
+  //       </CardSection>
+  //       </Card>
+  //           )
+  //   }
+  // }
+  // 
+  createBitcoinKey(){
+    const keypair = Bitcoin.ECPair.makeRandom();
+    const address = keypair.getAddress();
+    this.setState({bitcoinAdresss: address})
+    this.setState({keypairdata: keypair})
+    // console.log(keypair)
+    if(this.state.keypairdata != 'none'){
+      console.log(this.state.keypairdata.toWIF())
+    }
+  }
   
   render() {
+    const {bitcoinAdresss, keypairdata } = this.state;
+    
     return (
       <View>
         <Header headerText="Wallet"/>
-        <CardSection style={styles.container} >
-          <Text style={styles.instructions}>
+        <CardSection>
+          <Text>
             Welcome to the wallet, Beta, work in progress
           </Text>
         </CardSection>
+              <CardSection>
+                <Button onPress={()=> {this.createBitcoinKey()
+          }}>
+                  Create bitcoin key
+                </Button>
+              </CardSection>
+              <Text>
+              Public Key: 
+              </Text>
+              <Text>
+              {bitcoinAdresss}             
+             </Text>
+              <Text>
+              Private Key:  
+              </Text>
+              <Text>
+              { keypairdata != 'none' ? keypairdata.toWIF() : keypairdata }  
+              </Text>
+              <Text>
+              Other:
+              </Text>
+              <Text>
+              {keypairdata != 'none' ? keypairdata.getPublicKeyBuffer() : keypairdata}
+              </Text>
+              <Text>
+              prefix: 
+              </Text>
+              <Text>
+              {keypairdata != 'none' ? keypairdata.getNetwork().messagePrefix : keypairdata} 
+              </Text>
+
       </View>
     );
   }
